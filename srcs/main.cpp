123eb163862bb46c630b706321391d30f4dfbd5d
Nicolas.Keita <nicolas.keita@epitech.eu>
2019-11-18 16:57:45 +0100

    First commit

---
+++ srcs/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** main.cpp
+** File description:
+**
+*/
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello world !" << std::endl;
+    return 0;
+}
