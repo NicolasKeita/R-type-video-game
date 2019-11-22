5dd332bea0278713aed449e7c3028403cbaf9c57
Nicolas.Keita <nicolas.keita@epitech.eu>
2019-11-22 07:55:14 +0100

    Added tests to network lib

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # R-type-video-game
+
+[![Build Status](https://travis-ci.com/NicolasKeita/R-type-video-game.svg?token=yCU9eZUj8esevSHWuiuJ&branch=master)](https://travis-ci.com/NicolasKeita/R-type-video-game)
+
 A basic 2D video game
 
 Prerequisites :
--- lib/uti/my_network/tests/test_1_network_server_client_multiple_messages.sh
+++ lib/uti/my_network/tests/test_1_network_server_client_multiple_messages.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd ./test_1_source_code/
+cd "$1/test_1_source_code/" || exit 42
 g++ serv.cpp ../../ServerWrapper.cpp -l pthread  -o serv -I ../../
 g++ client.cpp ../../ClientWrapper.cpp -l pthread -I ../../../my_string/ -I ../../ -o client
 ./serv > /dev/null 2>&1 &
 ./client >> ../log_client.txt 2>&1 &
-sleep 3
+sleep 4
 ps -ef | grep -w ./serv | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep -w ./client | grep -v grep | awk '{print $2}' | xargs kill
 FIRST_TIME_RESULT=$(grep "FIRST TEST SUCCESSFULLY COMPLETED" ../log_client.txt)
@@ -14,9 +14,9 @@ rm ../log_client.txt
 rm ./serv
 rm ./client
 if [ -n "$FIRST_TIME_RESULT" ] && [ -n "$SECOND_TIME_RESULT" ] ; then
-echo "[Test Network] Test_1 passed."
+#echo "[Test Network] Test_1 passed."
 exit 0
 else
-echo "[Test Network] Test_1 failed"
+#echo "[Test Network] Test_1 failed"
 exit 1
 fi
