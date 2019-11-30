5d51bbb62654b02c9b02f60ad1e8385e58848c88
Lilian Brun <Lilian@localhost.localdomain>
2019-11-30 20:15:05 +0100

    Adding code restructured for window and background + removed some spaceships

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,10 +7,13 @@
 
 #include <iostream>
 #include "background.hpp"
+#include "Graphic.hpp"
 
 int main(int argc, char **argv, char **env)
 {
 	Background Background;
-	Background.display();
+	GraphicWrapper GraphicWrapper;
+	GraphicWrapper.createWindows(1920, 1080);
+	Background.display(&GraphicWrapper.window);
 	return 0;
 }
