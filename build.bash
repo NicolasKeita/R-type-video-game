4f50fb97640de57336bf4b3938a47dc7be1718c6
Nicolas Keita <nicolas.keita@epitech.eu>
2019-12-23 19:17:56 +0100

    changed compilater version

--- build.bash
+++ build.bash
@@ -2,7 +2,8 @@
 
 mkdir -p ./build/
 cd ./build/ || exit
-conan install ../ --build=missing -s compiler=gcc -s compiler.version=5.1 -s compiler.libcxx=libstdc++11 -s build_type=Debug -g virtualenv
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan install ../ --build=missing -s compiler=gcc -s compiler.version=9.2 -s compiler.libcxx=libstdc++11 -s build_type=Release -g virtualenv
 source activate.sh
 cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build ./ -j "$(nproc --all)"
@@ -11,4 +12,4 @@ cd ../
 
 
 #windows 10
-conan install ../ --build=missing -s compiler="Visual Studio" -s compiler.version=16 -s compiler.cppstd=14 -s build_type=Debug -g virtualenv
+conan install ../ --build=missing -s compiler="Visual Studio" -s compiler.version=16 -s compiler.cppstd=14 -s build_type=Release -g virtualenv
