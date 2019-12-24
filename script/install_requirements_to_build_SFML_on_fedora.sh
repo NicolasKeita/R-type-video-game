bae38ab94034a787cafbbf74481bdfb539865b90
Nicolas Keita <nicolas.keita@epitech.eu>
2019-12-24 07:54:39 +0100

    Added more dependancies

--- script/install_requirements_to_build_SFML_on_fedora.sh
+++ script/install_requirements_to_build_SFML_on_fedora.sh
@@ -2,3 +2,6 @@ sudo dnf install libXrandr-devel #X11
 sudo dnf install freeglut-devel #openGL
 sudo dnf install systemd-devel #libudev
 sudo dnf install SFML-devel
+sudo dnf install openal
+sudo dnf install g++
+sudo dnf install libtool
