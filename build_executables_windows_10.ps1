a3a500ebfa1207e106b6c8b11707cc44eceb68d3
Nicolas KEITA <nicolas.keita@epitech.eu>
2019-12-24 09:22:00 +0100

    Script to build on Windows able to do the same as Linux

--- build_executables_windows_10.ps1
+++ build_executables_windows_10.ps1
@@ -1,1 +1,7 @@
+mkdir build
+cd build
 conan install ../ --build=missing -s compiler="Visual Studio" -s compiler.version=16 -s compiler.cppstd=14 -s build_type=Release -g virtualenv
+cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake --build ./
+cp -r ../bin/Linux/assets ./bin
+echo -e "\n\nExecutables are located in build/bin/\n"
