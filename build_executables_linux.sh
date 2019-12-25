2e7f2f3ec5b42a29b714e51cd2a8855fa0ccb58f
Nicolas Keita <nicolas.keita@epitech.eu>
2019-12-25 13:11:41 +0100

    added generic gcc version

--- build_executables_linux.sh
+++ build_executables_linux.sh
@@ -3,8 +3,7 @@
 mkdir -p ./build/
 cd ./build/ || exit
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-#change 9.2 to your gcc version (gcc --version)
-conan install ../ --build=missing -s compiler=gcc -s compiler.version=9.2 -s compiler.libcxx=libstdc++11 -s build_type=Release -g virtualenv
+conan install ../ --build=missing -s compiler=gcc -s compiler.version="$(gcc -v 2>&1 | tail -1 | awk '{print $3}' | cut -d "." --fields=1-2)" -s compiler.libcxx=libstdc++11 -s build_type=Release -g virtualenv
 cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build ./ -j "$(nproc --all)"
 cp -rf ../bin/Linux/assets ./bin
