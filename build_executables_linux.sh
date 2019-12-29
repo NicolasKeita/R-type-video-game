b1fdbcc15c0bfb09bd5ad2fd6e9874f18773e3a8
Nicolas Keita <nicolas.keita@epitech.eu>
2019-12-29 19:27:01 +0100

    hotfix compilation, shared libsfml is now linked

--- build_executables_linux.sh
+++ build_executables_linux.sh
@@ -3,7 +3,7 @@
 mkdir -p ./build/
 cd ./build/ || exit
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan install ../ --build=missing -s compiler=gcc -s compiler.version="$(gcc -v 2>&1 | tail -1 | awk '{print $3}' | cut -d "." --fields=1-2)" -s compiler.libcxx=libstdc++11 -s build_type=Release -g virtualenv
+conan install ../ --build=missing -s compiler=gcc -s compiler.version="$(gcc -v 2>&1 | tail -1 | awk '{print $3}' | cut -d "." --fields=1)" -s compiler.libcxx=libstdc++11 -s build_type=Release -g virtualenv
 cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build ./ -j "$(nproc --all)"
 cp -rf ../bin/Linux/assets ./bin
